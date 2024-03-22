a6893a086c0a020ed52a643c6a1b7085fbc31ae2
Drigo <161271906+Drigoneo@users.noreply.github.com>
2024-03-22 13:02:37 +0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 
-curl_output=$(curl -H "Content-type: application/json" -X POST --data '{"jsonrpc":"2.0","method":"eth_getBlockByNumber","params":["latest", false],"id":1}' https://RPCETHLAVA/)
+curl_output=$(curl -H "Content-type: application/json" -X POST --data '{"jsonrpc":"2.0","method":"eth_getBlockByNumber","params":["latest", false],"id":1}' https://eth1.lava.build/lava-referer-6d999553-a78d-4232-bd09-cb323fb246f3/)
 
 
 echo "$curl_output" >> Update.tsx
